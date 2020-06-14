5c44eaaa1ee4a4cf2a715ac8b14e2e197f8e286e
Yuhao Zhang <yuhao.zhang@MacBook-Pro.local>
2020-06-14 22:29:54 +0800

    add delete and update module

--- src/index.js
+++ src/index.js
@@ -1,77 +1,126 @@
-import React from 'react' ;
+import React, { useState, useEffect } from 'react' ;
 import ReactDOM from 'react-dom' ;
 import axios from 'axios' ;
 
-class TodoApp extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = { 
-        items: {}, 
-        text: '',
-        success: false,
-      };
-      this.handleChange = this.handleChange.bind(this);
-      this.handleSubmit = this.handleSubmit.bind(this);
+const TodoApp = () => {
+    const [ success , setSuccess ] = useState(false);
+    const [ text , setText ] = useState('');
+    const [ items , setItems ] = useState([]);
+    const [ edittext , setEdittext ] = useState('');
+
+    const handleInputOnFocus = (e) => {
+      setEdittext(e.target.value);
+    }
+    const handleInputNoFocus = (todo) => {
+      handleEdit(todo);
     }
 
-    componentDidMount(){
-      axios.get('http://localhost:3001/todos')
-      .then(response=>{
-        let data = response.data.map(data=>data.item);
-        console.log(data);
-        this.setState({items:data,success:true});
-      });
-    }
-   
-    render() {
-      return (
-        <div>
-          <h3>TODO-LIST</h3>
-          <ul>
-          {this.state.success?
-          this.state.items.map((item,index)=>(
-            <li key={item}>{item}</li>
-          ))
-          :
-          'loading......'}
-         </ul>
-          <form onSubmit={this.handleSubmit}>
-            <label htmlFor="new-todo">
-              What needs to be done?
-            </label>
-            <input
-              id="new-todo"
-              onChange={this.handleChange}
-              value={this.state.text}
-            />
-            <button>
-              Add #{this.state.items.length + 1}
-            </button>
-          </form>
-        </div>
-      );
-    }
-
-    handleChange(e) {
-      this.setState({ text: e.target.value });
-    }
-  
-    handleSubmit(e) {
-      e.preventDefault();
-      if (this.state.text.length === 0) {
-        return;
-      }
-      this.setState({success: false});
-      axios.post('http://localhost:3001/todos',{item:this.state.text})
+    const handleSubmit = () => {
+      axios.post('http://localhost:3001/todos',{item:text})
       .then(response=>{
         if(response.status===200){
           window.location.reload();
         }
       });
     }
+    
+    const getAllItemsFromAPI = () => {
+      return axios.get('http://localhost:3001/todos')
+      .then(response=>{
+        let data = response.data.map(data=>data.item);
+        return data ;
+      });
+    };
+
+    const handleDelete = (todo) => {
+      console.log(todo);
+      axios({
+        method: 'delete',
+        url: 'http://localhost:3001/todos',
+        data: {
+          item: todo
+        }
+      })
+      .then(response=>{
+        if(response.status===200){
+          window.location.reload();
+        }
+      });
+    }
+
+    const handleEdit = (todo) => {
+      axios({
+        method: 'put',
+        url: 'http://localhost:3001/todos',
+        data: {
+          item: todo,
+          newValue:edittext
+        }
+      })
+      .then(response=>{
+        if(response.status===200){
+          window.location.reload();
+        }
+      });
+    }
+
+    useEffect(()=>{
+      async function initItems(){
+        const initailState = await getAllItemsFromAPI();
+        console.log(initailState);
+        setItems(initailState);
+        setSuccess(true);
+      }
+      initItems();
+    },[]);
+    
+    return (
+      <div>
+        <h3>TODO-LIST</h3>
+        <ul>
+        {success?
+        items.map((todo,index)=>(
+          <div key={todo}>
+          <li>
+            <input 
+            id={todo} 
+            type="text" 
+            defaultValue={todo}
+            onFocus={handleInputOnFocus}
+            onBlur={handleInputNoFocus.bind(this,todo)}
+            onChange={ (e) => {setEdittext(e.target.value)}}>
+            </input>
+            <button variant="contained" color="secondary" onClick={handleEdit.bind(this,todo)} 
+            >
+              edit
+            </button>
+            <button variant="contained" color="secondary" onClick={handleDelete.bind(this,todo)}>
+              delete
+            </button>
+          </li>
+          </div>
+        ))
+        :
+        'loading......'}
+      </ul>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="new-todo">
+            What needs to be done?
+          </label>
+          <input
+            id="new-todo"
+            onChange={e => {setText(e.target.value)}}
+          />
+          <button>
+            Add #{items.length + 1}
+          </button>
+        </form>
+      </div>
+    );
   }
+
+ReactDOM.render(
+  <TodoApp />,
+  document.getElementById('todos-example')
+);
   
-  ReactDOM.render(
-    <TodoApp />,
-    document.getElementById('todos-example')
-  );
